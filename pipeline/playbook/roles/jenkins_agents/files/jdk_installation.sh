cf9d79ad2385cbae8a94193bd00394a583733e85
Zhiqi Lu <zhiqilu8470@gmail.com>
2022-12-01 10:36:42 +0800

    fix: jenkins ansible script typo

--- pipeline/playbook/roles/jenkins_agents/files/jdk_installation.sh
+++ pipeline/playbook/roles/jenkins_agents/files/jdk_installation.sh
@@ -4,5 +4,3 @@ cd /usr/local/java
 sudo tar xvf /usr/local/java/OpenJDK11U-jdk_x64_linux_hotspot_11.0.17_8.tar.gz
 
 sudo update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/jdk-11.0.17+8/bin/java" 1
-
-return 0
