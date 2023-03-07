aafa0a4af2076192368052b6d0c4f90358008252
Zhiqi Lu <zhiqilu8470@gmail.com>
2023-03-07 17:33:07 +0800

    feat: add harbor instance in aws pipeline infrastructure

--- aws/infrastructure/modules/pipeline/main.tf
+++ aws/infrastructure/modules/pipeline/main.tf
@@ -26,22 +26,22 @@ resource "aws_instance" "jenkins_agent1" {
   }
 }
 
-resource "aws_instance" "sonarqube" {
-  ami = var.ami
-  instance_type = "t3.medium"
-  key_name = "lu-aws"
-  subnet_id = var.public_subnet
-  vpc_security_group_ids = var.public_security_groups
-  private_ip = "10.0.100.20"
-  tags = {
-    Name = "${var.prefix}-sonarqube"
-    Owner = var.Owner
-  }
-}
+# resource "aws_instance" "sonarqube" {
+#   ami = var.ami
+#   instance_type = "t3.medium"
+#   key_name = "lu-aws"
+#   subnet_id = var.public_subnet
+#   vpc_security_group_ids = var.public_security_groups
+#   private_ip = "10.0.100.20"
+#   tags = {
+#     Name = "${var.prefix}-sonarqube"
+#     Owner = var.Owner
+#   }
+# }
 
 resource "aws_instance" "nexus_repo" {
   ami = var.ami
-  instance_type = "t3.medium"
+  instance_type = "t3.small"
   key_name = "lu-aws"
   subnet_id = var.public_subnet
   vpc_security_group_ids = var.public_security_groups
@@ -53,6 +53,24 @@ resource "aws_instance" "nexus_repo" {
 
   tags = {
     Name = "${var.prefix}-nexus"
+    Owner = var.Owner
+  }
+}
+
+resource "aws_instance" "harbor" {
+  ami = var.ami
+  instance_type = "t3.small"
+  key_name = "lu-aws"
+  subnet_id = var.public_subnet
+  vpc_security_group_ids = var.public_security_groups
+  private_ip = "10.0.100.40"
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  tags = {
+    Name = "${var.prefix}-harbor"
     Owner = var.Owner
   }
 }
