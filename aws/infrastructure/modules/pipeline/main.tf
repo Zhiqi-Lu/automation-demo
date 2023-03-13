aa6b82de66828bf282e11f15407bdf7236902bf9
Zhiqi Lu <zhiqilu8470@gmail.com>
2023-03-13 18:04:33 +0800

    fix: pipeline ec2 instance size and docker private registry addresses

--- aws/infrastructure/modules/pipeline/main.tf
+++ aws/infrastructure/modules/pipeline/main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "jenkins_controller" {
   subnet_id = var.public_subnet
   vpc_security_group_ids = var.public_security_groups
   private_ip = "10.0.100.10"
+  user_data = filebase64("${path.module}/app_init.sh")
 
   tags = {
     Name = "${var.prefix}-joi-jenkins-controller"
@@ -19,6 +20,7 @@ resource "aws_instance" "jenkins_agent1" {
   subnet_id = var.public_subnet
   vpc_security_group_ids = var.public_security_groups
   private_ip = "10.0.100.11"
+  user_data = filebase64("${path.module}/app_init.sh")
 
   tags = {
     Name = "${var.prefix}-joi-jenkins-agent"
@@ -41,11 +43,12 @@ resource "aws_instance" "jenkins_agent1" {
 
 resource "aws_instance" "nexus_repo" {
   ami = var.ami
-  instance_type = "t3.small"
+  instance_type = "t3.medium"
   key_name = "lu-aws"
   subnet_id = var.public_subnet
   vpc_security_group_ids = var.public_security_groups
   private_ip = "10.0.100.30"
+  user_data = filebase64("${path.module}/app_init.sh")
 
   root_block_device {
     volume_size = 20
@@ -59,11 +62,12 @@ resource "aws_instance" "nexus_repo" {
 
 resource "aws_instance" "harbor" {
   ami = var.ami
-  instance_type = "t3.small"
+  instance_type = "t3.medium"
   key_name = "lu-aws"
   subnet_id = var.public_subnet
   vpc_security_group_ids = var.public_security_groups
   private_ip = "10.0.100.40"
+  user_data = filebase64("${path.module}/app_init.sh")
 
   root_block_device {
     volume_size = 20
